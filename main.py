51383116af4ea398b4c84c1df7afe0c6b22538bf
kerby <lovincekerby99@gmail.com>
2022-11-27 20:36:22 -0300

    tentei finalizar a parte para encontra os ckpt

--- main.py
+++ main.py
@@ -29,20 +29,67 @@ def removeLine(data, trans):
     return aux
 
 # remove o checkpoint e os commits salvos do arquivo redo na memória
-
+def removeCheck(data, unfinished):
+    temp = str(unfinished[12:-3])
+    temp = temp.split(",")
+    finished = []
+    
+    for i in range(len(data)):
+        if data[i] == unfinished:
+            break
+        if data[i] != "<crash>" and "," not in data[i][1:-2]:
+            trans = (data[i[1:-2]]).split(" ")[1]
+            if trans not in temp and trans not in finished:
+                finished.append(trans)
     
     
+    for t in finished:
+        data = removeLine(data, t)
+        
+    data.pop(data.index(unfinished))
+    data.reverse()
+    data.remove("<End CKPT> \n")
+    return data
+
+# encontre o checkpoint finalizado no arquivo log
+
+def findCheckpoint(data):
+    stack = []
+    for line in data:
+        if line == "< End CKPT> \n":
+            stack.append(True)
+        if len(stack) > 0:
+            if stack[-1] and line[:11] == "<Start CKPT>":
+                data.reverse()
+                data = removeCheck(data, line)
+                stack.pop()
+                
+    return data
 
 
 
-# # Connect to your postgres DB
-# # conn = psycopg2.connect("dbname=test user=postgres")
 
-# # Open a cursor to perform database operations
-# cur = conn.cursor()
+data = getLog("entradaLog")
 
-# # Execute a query
-# cur.execute("SELECT * FROM my_data")
+print(data)
 
-# # Retrieve query results
-# records = cur.fetchall()
+data = findCheckpoint(data)
+
+print(data)
+
+
+    
+
+
+
+# Connect to your postgres DB
+# conn = psycopg2.connect("dbname=test user=postgres")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM data")
+
+# Retrieve query results
+records = cur.fetchall()
